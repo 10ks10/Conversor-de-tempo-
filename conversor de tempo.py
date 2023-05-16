963e793ff51e775a205d7775972155d727ad49aa
10ks10 <124063409+10ks10@users.noreply.github.com>
2023-05-16 12:21:17 -0300

    Add files via upload

---
+++ conversor de tempo.py
@@ -0,0 +1,5 @@
+horas = int(input("quantas horas deseja converter? \n"))
+minutos = horas * 60
+segundos = minutos * 60
+
+print (horas, "horas tem ", minutos, "minutos, ou seja, tem ", segundos, "segundos")
